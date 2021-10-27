b82d62e2867d491456a94a01818f9dff62e8800e
gitaccount999 <92069984+gitaccount999@users.noreply.github.com>
2021-10-27 15:13:39 +0900

    test

---
+++ scann/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  skrwt
+//
+//  Created by tambi on 9/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
